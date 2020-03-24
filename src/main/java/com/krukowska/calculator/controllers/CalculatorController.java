3c53a434f48c70355e94b529a7de6b1020e0383f
Edyta <krukowskaedyta@gmail.com>
2020-03-24 09:13:54 +0100

    Logs

--- src/main/java/com/krukowska/calculator/controllers/CalculatorController.java
+++ src/main/java/com/krukowska/calculator/controllers/CalculatorController.java
@@ -2,17 +2,21 @@ package com.krukowska.calculator.controllers;
 
 import com.krukowska.calculator.model.EquationResult;
 import com.krukowska.calculator.services.CalculatorService;
+import lombok.extern.log4j.Log4j2;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.websocket.server.PathParam;
 
 @RestController
+@Log4j2
 public class CalculatorController {
 
     CalculatorService calculatorService;
+    private static final Logger log = LogManager.getLogger(CalculatorController.class);
 
     @Autowired
     public CalculatorController(CalculatorService calculatorService) {
@@ -21,6 +25,7 @@ public class CalculatorController {
 
     @GetMapping(value = "/v1/add")
     public EquationResult addNumbers(@PathParam("val1") int val1, @PathParam("val2") int val2) {
+        log.info("Result method accessed");
         return calculatorService.add(val1, val2);
     }
 
