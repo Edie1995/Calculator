7f5519fb99f779f860aac732ac21675c2d227da9
Edyta <krukowskaedyta@gmail.com>
2020-03-25 11:18:13 +0100

    Logs

--- Dockerfile
+++ Dockerfile
@@ -1,6 +1,7 @@
 FROM docker.io/openjdk:11-jre-slim
-RUN addgroup edyta && useradd edyta -g edyta
-USER edyta:edyta
 ARG JAR_FILE=target/*.jar
+RUN addgroup edyta && useradd edyta -g edyta
+RUN chown -R  edyta:edyta /var/log
+USER edyta:edyta
 COPY ${JAR_FILE} app.jar
 ENTRYPOINT ["java","-jar","/app.jar"]
--- src/main/java/com/krukowska/calculator/CalculatorApplication.java
+++ src/main/java/com/krukowska/calculator/CalculatorApplication.java
@@ -1,13 +1,20 @@
 package com.krukowska.calculator;
 
+import com.krukowska.calculator.controllers.CalculatorController;
+import lombok.extern.log4j.Log4j2;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@Log4j2
 public class CalculatorApplication {
-
+    private static final Logger log = LogManager.getLogger(CalculatorApplication.class);
     public static void main(String[] args) {
+        log.error("odpalilem sie !!!");
         SpringApplication.run(CalculatorApplication.class, args);
+
     }
 
 }
