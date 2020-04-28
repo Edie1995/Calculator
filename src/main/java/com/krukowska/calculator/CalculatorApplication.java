3c9c2fa8ce0b32003eb960eda4fa1e66bdc1b494
edyta <edyta.krukowska@nokia.com>
2020-04-28 10:13:44 +0200

    added prometheus

--- src/main/java/com/krukowska/calculator/CalculatorApplication.java
+++ src/main/java/com/krukowska/calculator/CalculatorApplication.java
@@ -6,8 +6,10 @@ import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.metrics.export.prometheus.EnablePrometheusMetrics;
 
 @SpringBootApplication
+@EnablePrometheusMetrics
 public class CalculatorApplication {
     public static void main(String[] args) {
         SpringApplication.run(CalculatorApplication.class, args);
