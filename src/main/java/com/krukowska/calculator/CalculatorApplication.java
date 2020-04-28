eecb83b121645fc98f6671f9a9f3a9506b7db49d
edyta <edyta.krukowska@nokia.com>
2020-04-28 11:56:26 +0200

    fixed pom.xml

--- src/main/java/com/krukowska/calculator/CalculatorApplication.java
+++ src/main/java/com/krukowska/calculator/CalculatorApplication.java
@@ -1,15 +1,9 @@
 package com.krukowska.calculator;
 
-import com.krukowska.calculator.controllers.CalculatorController;
-import lombok.extern.log4j.Log4j2;
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.metrics.export.prometheus.EnablePrometheusMetrics;
 
 @SpringBootApplication
-@EnablePrometheusMetrics
 public class CalculatorApplication {
     public static void main(String[] args) {
         SpringApplication.run(CalculatorApplication.class, args);
